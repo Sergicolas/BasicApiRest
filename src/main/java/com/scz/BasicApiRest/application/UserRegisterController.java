22944379e3cf9ef86ff4eeef3a1d77b8751662e5
Serginho <sergicolas@hotmail.com>
2022-03-30 17:10:07 +0100

    Swagger Commit

--- src/main/java/com/scz/BasicApiRest/application/UserRegisterController.java
+++ src/main/java/com/scz/BasicApiRest/application/UserRegisterController.java
@@ -2,8 +2,11 @@ package com.scz.BasicApiRest.application;
 
 import com.scz.BasicApiRest.models.UserRegister;
 import com.scz.BasicApiRest.repository.UserRegisterRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,33 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value="/api")
+@CrossOrigin(origins = "*")
+@Api(value="API REST User Register")
 public class UserRegisterController {
 
     @Autowired
     UserRegisterRepository userRegisterRepository;
 
     @GetMapping("/users")
+    @ApiOperation(value="Return a list of Users")
     public List<UserRegister> findAllUsers(){
         return userRegisterRepository.findAll();
     }
 
+
     @GetMapping("/user/{id}")
+    @ApiOperation(value="Return a single User")
     public UserRegister findUserById(@PathVariable(value = "id") long id){
         return userRegisterRepository.findUserById(id);
     }
 
     @PostMapping("/user")
+    @ApiOperation(value="Register a new User")
     public UserRegister saveUser(@RequestBody UserRegister userRegister){
         return userRegisterRepository.save(userRegister);
     }
 
     @PutMapping("/user")
+    @ApiOperation(value="Update an User")
     public UserRegister updateUser(@RequestBody UserRegister userRegister){
         return userRegisterRepository.save(userRegister);
     }
 
     @DeleteMapping("/user/{id}")
+    @ApiOperation(value="Delete an User by id")
     public void deleteUser(@PathVariable(value = "id") long id){
         userRegisterRepository.deleteById(id);
     }
